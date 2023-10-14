1b181c073ee1e965b10a7689b3f6428938330442
999-andreas <andreas.chatel5@gmail.com>
2023-10-14 17:44:02 +0200

    test d'acces a la bdd

--- app/public/index.php
+++ app/public/index.php
@@ -1,14 +1,58 @@
 <?php
-/*
-use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+class Database{
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};*/
+    private static $dns;
+    private static $user;
+    private static $password;
+    private static $database;
+
+    /*public function __construct()
+    {
+        self::$dns = "mysql:host=db;dbname=sf-react;port=3306"; // À changer selon vos configurations
+        self::$user = "root"; // À changer selon vos configurations
+        self::$password = ""; // À changer selon vos configurations
+        try {
+            self::$database = new PDO(self::$dns, self::$user, self::$password);
+            self::$database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo "Erreur de connexion à la base de données: " . $e->getMessage();
+            die();
+        }
+    }*/
+
+    public static function getInstance() {
+        if (!self::$database) {
+            self::$dns = "mysql:host=db;dbname=sf-react;port=3306"; // À changer selon vos configurations
+            self::$user = "root"; // À changer selon vos configurations
+            self::$password = ""; // À changer selon vos configurations
+            try {
+                self::$database = new PDO(self::$dns, self::$user, self::$password);
+                self::$database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch(PDOException $e) {
+                echo "Erreur de connexion à la base de données: " . $e->getMessage();
+                die();
+            }
+        }
+        return self::$database;
+    }
+
+    public function GetUserById($user_id) {
+        $database = self::getInstance();
+        $query = "SELECT * FROM user  WHERE id = ?";
+        $statement = $database->prepare($query);
+        $statement->bindParam(1, $user_id, PDO::PARAM_INT);
+        $statement->execute();
+        $user = $statement->fetch(PDO::FETCH_ASSOC);
+        return $user;
+    }
+} 
+
+$db = new Database();
+
+$user = $db->GetUserById(1);
+
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,6 +61,8 @@ return function (array $context) {
     <title>Document</title>
 </head>
 <body>
-    bonjour le monde
+    bonjour à tous 
+    <?php var_dump($user);?>
 </body>
 </html>
+
